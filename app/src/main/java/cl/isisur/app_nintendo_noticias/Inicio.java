fa1ea2caf588d445b3caca3323f4982ffabd8d2f
matia <matiasvera98@hotmail.com>
2021-10-25 17:51:10 -0300

    25/10/2021 v006

--- app/src/main/java/cl/isisur/app_nintendo_noticias/Inicio.java
+++ app/src/main/java/cl/isisur/app_nintendo_noticias/Inicio.java
@@ -2,18 +2,29 @@ package cl.isisur.app_nintendo_noticias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Inicio extends AppCompatActivity {
     Button btCerrar;
+    String stUsuario = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        Context context = getApplicationContext();
+        Bundle intent = this.getIntent().getExtras();
+        if(intent !=null)
+        {
+            stUsuario = intent.getString("nombre");
+            Toast.makeText(context, "Bienvenido: "+stUsuario, Toast.LENGTH_SHORT).show();
+        }
         btCerrar=(Button) findViewById(R.id.btSalir);
         Intent intentUno = new Intent(this,MainActivity.class);
 
--- app/src/main/java/cl/isisur/app_nintendo_noticias/MainActivity.java
+++ app/src/main/java/cl/isisur/app_nintendo_noticias/MainActivity.java
@@ -2,24 +2,37 @@ package cl.isisur.app_nintendo_noticias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btIniciar;
+    EditText etNombre;
+    EditText etContrasena;
+    String Usuario;
+    String Contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btIniciar = (Button) findViewById(R.id.btiniciar);
+        etNombre = (EditText) findViewById(R.id.ETCorreo);
+        etContrasena = (EditText) findViewById(R.id.ETcontraseña);
         Intent intentUno = new Intent(this,Inicio.class);
 
         btIniciar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //etNombre.getText().toString();
+                //etContrasena.getText().toString();
+                intentUno.putExtra("nombre",etNombre.getText().toString());
+                intentUno.putExtra("contraseña",etContrasena.getText().toString());
                 startActivity(intentUno);
             }
         });
